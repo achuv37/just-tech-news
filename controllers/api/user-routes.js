635e6b4fca52b3d59c90213fa865999340283677
Achu Ajesh <achuv37@gmail.com>
2022-05-07 19:47:19 -0400

    Add homepage

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,10 +19,30 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-       attributes: { exclude: ['password'] },
-        where: {
-          id: req.params.id
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
         }
+      ]
       })
         .then(dbUserData => {
           if (!dbUserData) {
