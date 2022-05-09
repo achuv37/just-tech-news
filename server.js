fe37fad90e2cc130f017805f6a5fd3047b45000d
Achu Ajesh <achuv37@gmail.com>
2022-05-09 09:17:10 -0400

    add authentication

--- server.js
+++ server.js
@@ -5,9 +5,22 @@ const path = require('path');
 const app = express();
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-const PORT = process.env.PORT || 8001;
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const PORT = process.env.PORT || 3051;
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
